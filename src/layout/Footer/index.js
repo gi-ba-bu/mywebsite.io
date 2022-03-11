a9dff8458ec4f36ac9a4f670db3385f1a0f7678e
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-03-11 21:51:10 +0100

    style

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './style.css';
 
-const Footer = () => <span id="footer">&copy; Giacomo Baldo 2022. Built with React, CSS and HTML. </span>
+
+const Footer = () => {
+
+return(
+    <div id="footer">  
+        <span> &copy; Giacomo Baldo 2022. </span> 
+        <span> Built with <img id = "footerimages"style={{ height: "50px", width: "50px" }} src="../../images/reacticon.svg" alt="Sunrise" /> React, CSS and JSX. Deployed with Netlify. </span> 
+    </div>
+)}
 
 export default Footer;
