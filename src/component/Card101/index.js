0b759510a7728ed5d66e5ba18812b55aa7886879
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-04-30 14:56:40 +0200

    card 101 update description

--- src/component/Card101/index.js
+++ src/component/Card101/index.js
@@ -20,7 +20,7 @@ const Card101 = (prop) => {
         <div className='card-container'>
         <img className='card-image' onClick={handleClick} src="../../images/flourish.png" alt="react" />      
         <p className='card-text'>
-        An app that aims to reduce food waste by letting users share information about reduced food items. Made using Flask, PostgreSQL and React.  
+        A mobile-first app that aims to reduce food waste by letting users share information about reduced food items. It allows to upload pictures, add product details, and it integrates Google maps to pinpoint items on a map. Made using Flask, PostgreSQL and React.  
         </p>
         <p className='card-text' id='card-git-link'>
            <span className='add-padd-link'>Live website</span> <a className='arrow-a' onClick={handleClick}> &#8594; </a>
